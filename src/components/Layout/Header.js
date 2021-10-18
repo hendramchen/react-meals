0071db0f680fcef0ae7b1ace7a60a4778c73807e
Hendra Putu <hendra@slash.co>
2021-10-18 19:10:21 +0800

    add header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+import mealsImg from './../../assets/meals.jpeg';
+
+const Header = props => {
+  return <Fragment>
+    <header className={classes.header}>
+      <h1>React Meals</h1>
+      <HeaderCartButton />
+    </header>
+    <div className={classes['main-image']}>
+      <img src={mealsImg} alt="React meals" />
+    </div>
+  </Fragment>
+}
+
+export default Header;
